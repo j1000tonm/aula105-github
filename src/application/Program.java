015ad76a78ea4e658c238b15af88f0956e3c4674
j1000tonm <j1000tonm.jmb@gmail.com>
2020-01-19 23:04:57 -0300

    Mensagem de Bom dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Boa Noite!!!");
 		System.out.println("Boa Tarde!!!");
-
+		System.out.println("Bom Dia!!!");
 	}
-
 }
